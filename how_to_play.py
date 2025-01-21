51af51d11390272d2d432dd4b089824b4a066dfe
Hazel Mina <minahazel2023@yahoo.com>
2025-01-21 10:14:59 +0000

    Added how_to_play.py

---
+++ how_to_play.py
@@ -0,0 +1,8 @@
+#To play the text-based adventure game, follow these steps:
+
+#Step 1: Run the Game in VS Code
+#Open VS Code.
+
+#Navigate to your project folder (where TBAG.py is saved).
+#Open the terminal in VS Code by pressing Ctrl + ~.
+#Use the command to move to your project directory (if needed)
